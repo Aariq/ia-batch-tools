4c065dc09d4d8fd698d3913e77b42c2f8bc71b00
Aariq <scottericr@gmail.com>
2019-07-01 13:29:56 -0400

    cleaned up UI code

--- ui.R
+++ ui.R
@@ -29,8 +29,6 @@ shinyUI(fluidPage(sidebarLayout(
   mainPanel(
 
     tabsetPanel(type = "tabs", id = "tabs",
-                
-                #this tab would have the RT deviation plotly plot and interactive table
                 tabPanel("RT",
                          h2("RT Consistency"),
                          p("Compounds sorted by standard deviation of retention time.  If you are inconsistent about choosing the correct peak, or if a compound has a naturally variable retention time, it will show up higher up and on earlier pages in these plots. Use the 'Page' input to look through all compounds. Hover over points to view details and select points using the selection tools on the plot to generate a table."),
@@ -40,22 +38,16 @@ shinyUI(fluidPage(sidebarLayout(
                          p("Select datapoints above with one of the selection tools to populate this table"),
                          dataTableOutput("brush")),
                 
-                #this tab would have a simple table of compounds sorted by mean Q-value
                 tabPanel("Q Value",
-                         # h2("Q Values"),
-                         # p("This table shows mean Q value for each compound across samples along with the Q value for each compound detected in each sample. You can also sort by other columns by clicking on them.  For example, you may spot individual sample/compound combinations with very low Q-values when sorting by q_val"),
-                         # dataTableOutput("qtable"),
-                         h2("Idea for visualization for Q-values:"),
-                         plotlyOutput("qplot", height = "600px"),
-                         p("A heat map with sample on the x-axis, sorted by average q-value [over all compounds] for that sample.  Compounds [with the lowest Q-values] on the y-axis, arranged by mean q-value for the compound.  That way shitty compounds and shitty samples would group together, but a unusually low q-value would stand out. Worth a try?")),
+                         h2("Q-values Consistency"),
+                         p("A heat map with sample on the x-axis, sorted by average Q-value [over all compounds] for that sample.  Compounds on the y-axis are arranged by mean Q-value for the compound.  Use to spot samples with missing peaks, samples with poor integration, and problematic samples."),
+                         plotlyOutput("qplot", height = "600px")),
                 
-                #this tab would have some diagnostics about peak width?
                 tabPanel("Width",
                          h2("Peak Width Consistency"),
                          p("Compound peak widths sorted by standard deviation of peak width.  This is helpful for spotting inconsistency in manual integration of broad peaks or peaks that are sometimes cut off by overlap with another compound"),
                          dataTableOutput("widthtable")),
                 
-                #This tab aims to detect problems with isomers
                 tabPanel("Isomer",
                          h2("Same peak, multiple compounds?"),
                          p("This table looks for compounds that have a very small difference in retention time from other compounds within the same sample.  That could indicate that IonAnalytics is integrating the same peak as two different compounds.  This often happens when isomers have similar MS signatures and IonAnalytics picks the same peak for all isomers.  After a problematic peak is identified, the IonAnalytics method can be edited to narrow the retention time window based on the start and end times of the correct hit."),
